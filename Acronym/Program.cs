591ebadfe6d2e3e2d0e1c10547b457c5f8667ac8
gkulik-6581 <66838666+gkulik-6581@users.noreply.github.com>
2020-12-01 15:33:06 -0500

    Update Program.cs
    First test got 2 letters. Trying now to get all first letters

--- Acronym/Program.cs
+++ Acronym/Program.cs
@@ -6,19 +6,22 @@ namespace Acronym
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter a phrase or group of words and press return. I will bive you its acronym.");
+            Console.WriteLine("Enter a phrase or group of words and press return. I will give you its acronym.");
             string userInput = Console.ReadLine();
 
             string finalAcronym = "";
+            char firstLetter = userInput[0];
 
+            for (int i = 0; i < userInput.Length; i++)
+                {
+                char findSpace = userInput[i];
+                if (findSpace == ' ')
+                    {
+                    finalAcronym = userInput.Substring(i + 1, 1);
+                    }
+                }
 
-
-
-
-
-
-
-            Console.WriteLine(finalAcronym);
+            Console.WriteLine(firstLetter + finalAcronym);
         }
     }
 }
